d84f0f2587b10a04ef1a54840dc3b3063f0769df
Alyson Takai <shtakai@users.noreply.github.com>
2018-10-07 21:22:20 +0700

    Feature/new pages (#3)
    * Add contact page
    
    * Re format on editor
    
    * Add 404 page
    
    * Import gorilla mux
    
    * Modify handlers
    
    * Modify to use gorilla mux
    
    * Add faq route/page
    
    * Add 404 route/page

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "<h1>welcome to fucked up</h1>")
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Fucked up moron on the death side.</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in fuck, please fuck an email "+
+		"to <a href=\"mailto:udon@example.com\">"+
+		"udon@example.com</a>.")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQU FUCKU FUCK YOU morons!</h1>"+
+		"<h2>Are you sure to die? [Y]/y</h2>")
+}
+
+func page404(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>404 NERD FUCKED UP DUDE</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handleFunc)
-	http.ListenAndServe(":3000", nil)
+	var h http.Handler = http.HandlerFunc(page404)
+	r := mux.NewRouter()
+	r.NotFoundHandler = h
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	http.ListenAndServe(":3000", r)
 }
