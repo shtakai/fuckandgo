07e8aa13a72ce5e2d2ab8a7dbba5a863720055c2
Alyson Takai <shtakai@gmail.com>
2018-10-20 19:40:52 +0700

    Add parsing form

--- controllers/users.go
+++ controllers/users.go
@@ -26,5 +26,9 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "This is fucked up temporary response, moron")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
 }
