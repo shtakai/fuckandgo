14aa306b23de0ca2a64176badd2dc985b69dff1e
Alyson Takai <shtakai@gmail.com>
2018-10-20 20:15:25 +0700

    Modify for serving static contents

--- main.go
+++ main.go
@@ -3,33 +3,10 @@ package main
 import (
 	"fmt"
 	"fuckandgo/controllers"
-	"fuckandgo/views"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-var (
-	homeView *views.View
-	contactView *views.View
-	faqView *views.View
-)
-
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(faqView.Render(w, nil))
-}
 
 func page404(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
@@ -37,25 +14,16 @@ func page404(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>404 NERD FUCKED UP DUDE</h1>")
 }
 
-func must(err error){
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrap", "views/faq.gohtml")
-
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
 	var h http.Handler = http.HandlerFunc(page404)
 	r := mux.NewRouter()
 	r.NotFoundHandler = h
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
-	r.HandleFunc("/faq", faq).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
+	r.Handle("/faq", staticC.Faq).Methods("GET")
 
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
