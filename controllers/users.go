aac88998d9a15641655001f150e4939841f9f16b
Alyson Takai <shtakai@gmail.com>
2018-10-20 16:08:44 +0700

    Implement new action

--- controllers/users.go
+++ controllers/users.go
@@ -1,3 +1,24 @@
 package controllers
 
-type Users struct {}
+import (
+	"fuckandgo/views"
+	"net/http"
+)
+
+func NewUsers() *Users {
+	return &Users{
+        NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+
+}
+
+// GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
