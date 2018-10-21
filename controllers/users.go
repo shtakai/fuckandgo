d34139d6ed70e4699efb109a4d2f8f5a2208cd90
Alyson Takai <shtakai@users.noreply.github.com>
2018-10-21 16:32:52 +0700

    Feature/signup (#8)
    * Add signup form
    
    * Add signup view
    
    * Apply bootstrap grid
    
    * Create Users controller
    
    * Move file
    
    * Implement new action
    
    * Modify to use users controller
    
    * Add POST stub
    
    * Add route w/methods
    
    * Add action/method on form
    
    * Add parsing form
    
    * Add parsing form w/schema
    
    * Refactor extract parseForm function
    
    * Move templates
    
    * Create controller for static html
    
    * Modify for serving static contents
    
    * Remove fmt (no need)
    
    * Refactor add dir/ext to simplify reading file
    
    * Remove no need

---
+++ controllers/users.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"fmt"
+	"fuckandgo/views"
+	"net/http"
+)
+
+func NewUsers() *Users {
+	return &Users{
+        NewView: views.NewView("bootstrap", "users/new"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+type SignupForm struct {
+	Email string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, "Email is", form.Email)
+	fmt.Fprintln(w, "Password is", form.Password)
+}
--- main.go
+++ main.go
@@ -2,30 +2,11 @@ package main
 
 import (
 	"fmt"
-	"fuckandgo/views"
+	"fuckandgo/controllers"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-var homeView *views.View
-var contactView *views.View
-var faqView *views.View
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(faqView.Render(w, nil))
-}
 
 func page404(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
@@ -33,22 +14,18 @@ func page404(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>404 NERD FUCKED UP DUDE</h1>")
 }
 
-func must(err error){
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	staticC := controllers.NewStatic()
+	usersC := controllers.NewUsers()
 
 	var h http.Handler = http.HandlerFunc(page404)
 	r := mux.NewRouter()
 	r.NotFoundHandler = h
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
+	r.Handle("/faq", staticC.Faq).Methods("GET")
+
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
 }
