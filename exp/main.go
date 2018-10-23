cf4a40fe095df2565dde8f7e9f44c111e65a047e
Alyson Takai <shtakai@users.noreply.github.com>
2018-10-23 14:18:44 +0700

    Feature/data store (#9)
    * Modify to connect postgres
    
    * Add sample SQL
    
    * Remove password
    
    * Add insertion users
    
    1. the first one is just inserting
    2. the second one is make query and execute and grab id
    
    * Add selection and grab attributes
    
    * Add grabbing rows by db.Query
    
    * Add insertion orders
    
    * Modify to connect with gorm
    
    * Add user model
    
    * Add automigration
    
    * Set log mode true
    
    * Add user insertion w/ stdin
    
    * Add selection w/ Where
    
    * Add multiple selection w/ Find
    
    * Add migration of Order
    
    * Add insertion Orders
    
    * Add relation to User <- Orders

--- exp/main.go
+++ exp/main.go
@@ -1,27 +1,67 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
 
-func main() {
-	m := make(map[string]int)
-	m["AveKilled"] = 1
-	m["TrampKillCount"] = 100
-	t, err := template.ParseFiles("hell.gohtml")
-	if err != nil {
-		panic(err)
-	}
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	dbname = "fuckandgo_dev"
+)
 
-	data := struct {
-		Name  string
-		Moron string
-		Map   map[string]int
-	}{"Fucked Sucked", "AVE SINxo", m}
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
+type User struct {
+	gorm.Model
+	Name string
+	Email string `gorm:"not null;unique_index"`
+	Orders []Order
 }
+
+type Order struct {
+	gorm.Model
+	UserId uint
+	Amount int
+	Description string
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected to DB")
+
+	defer db.Close()
+
+	db.LogMode(true)
+	db.AutoMigrate(&User{}, &Order{})
+
+	var user User
+	db.Preload("Orders").First(&user)
+	if db.Error != nil {
+		panic(db.Error)
+	}
+
+	fmt.Println("email:", user.Email)
+	fmt.Println("name:", user.Name)
+	fmt.Println("orders:", user.Orders)
+
+}
+
+//func createOrder(db *gorm.DB, user User, amount int, desc string) {
+//	db.Create(&Order{
+//		UserId: user.ID,
+//		Amount: amount,
+//		Description: desc,
+//	})
+//	if db.Error != nil {
+//		panic(db.Error)
+//	}
+//}
